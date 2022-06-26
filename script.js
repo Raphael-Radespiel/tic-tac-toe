a7b3a3e93624657ab3753ce923c426298474eb9f
Raphael Radespiel <contatoraphael1701@gmail.com>
2022-06-26 11:30:52 -0300

    Began work with responsive design and created the game board I created some responsive css media queries. They might not be the most optimized but they get the job done, changing the content style to fit screens smaller than 40rem in portrait mode and screens smaller than 60rem in landscape mode. I also created a function that resets the display class so it has different style, and that adds the tic-tac-toe button elements. It also sets the game mode depending on the setup button pressed.

--- script.js
+++ script.js
@@ -1,15 +1,62 @@
 const gameDisplay = document.querySelector('#display');
 
+let isPlayingCPU = false;
+
 function setUpSelection(){
   gameDisplay.classList.add('flex-box-one'); 
-  
+  gameDisplay.classList.remove('flex-box-two');
+
   let divLeft = document.createElement('div');
   let divRight = document.createElement('div');
   
-  gameDisplay.append(divLeft, divRight);
-
   divLeft.textContent = 'Human vs Human';
   divRight.textContent = 'Human vs Computer';
+
+  gameDisplay.append(divLeft, divRight);
+
+  divLeft.addEventListener('click', () => setUpGame(false));
+  divRight.addEventListener('click', () => setUpGame(true));
+}
+
+function setUpGame(gameMode){ 
+  // Adjust class of display
+  gameDisplay.classList.add('flex-box-two');
+  gameDisplay.classList.remove('flex-box-one');
+
+  // Removes previous state's elements
+  clearDisplay();
+
+  // Sets game mode and corresponding text
+  (gameMode) ? isPlayingCPU = true : isPlayingCPU = false;
+
+  let divMode = document.createElement('div');
+  (isPlayingCPU) ? divMode.textContent = 'Human vs Computer' : 
+      divMode.textContent = 'Human vs Human';
+
+  gameDisplay.append(divMode);
+
+  // Creates the tic-tac-toe board
+  let gameBoard = document.createElement('div');
+  gameBoard.classList.add('board-game');
+
+  for(let i = 0; i < 3; i++){  
+    let boardRow = document.createElement('div');
+    boardRow.classList.add('row');
+    boardRow.id = `row-${i}`;
+    for(let j = 0; j < 3; j++){
+      let boardColumn = document.createElement('div');
+      boardColumn.classList.add('col');
+      boardColumn.id = `col-${j}`;
+      boardRow.append(boardColumn);  
+    }
+    gameBoard.append(boardRow);
+  }
+
+  gameDisplay.append(gameBoard);
+}
+
+function clearDisplay(){
+  gameDisplay.innerHTML = '';
 }
 
 setUpSelection();
